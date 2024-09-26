3c84511da77fdcf7867278f4d28db085239b2d4d
Jonathan Bursztyn <jonathan@bursztyn.io>
2024-09-26 18:52:56 +0100

    Fixes

--- src/App.js
+++ src/App.js
@@ -4,18 +4,17 @@ function App() {
   return (
     <div className="App">
       <h1>To-Do List</h1>
-      <input type="text" placeholder="Add a new task" />
-      <button>Add Task</button>
-
-      <div>
+      <div className="input-container">
+        <input type="text" placeholder="Add a new task" />
+        <button>Add Task</button>
+      </div>
+      <div className="sort-container">
         <label>Sort by: </label>
         <select>
           <option value="alphabetical">Alphabetical</option>
           <option value="byTime">By Time</option>
         </select>
       </div>
-
-      <ul>{/* Placeholder for tasks */}</ul>
     </div>
   );
 }
